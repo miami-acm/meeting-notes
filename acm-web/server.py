a8e1306e9fde4b8d3522b080313295b5a4a198c8
John Meyer <jmm2016@ymail.com>
2018-02-22 16:18:50 -0500

    Update server script
    Add PEP8 and shebang

--- acm-web/server.py
+++ acm-web/server.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-	return "Hello, world!"
+    return 'Hello, world!'
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
