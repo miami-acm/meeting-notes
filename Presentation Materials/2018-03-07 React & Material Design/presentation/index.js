3e96c1211ea9205538ecde9f2021c94e84753a25
John Meyer <0x326@users.noreply.github.com>
2018-03-07 15:41:03 -0500

    Make simple visual aid

--- Presentation Materials/2018-03-07 React & Material Design/presentation/index.js
+++ Presentation Materials/2018-03-07 React & Material Design/presentation/index.js
@@ -33,38 +33,27 @@ const theme = createTheme({
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={['zoom', 'slide']} transitionDuration={500} theme={theme}>
-        <Slide transition={['zoom']} bgColor='primary'>
+      <Deck transition={['fade']} transitionDuration={500} theme={theme}>
+        <Slide bgColor='primary'>
           <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
-            Spectacle Boilerplate
+            Welcome to ACM!
           </Heading>
           <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
-            open the presentation/index.js file to get started
+            Today's Topic: React & Material Design
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor='tertiary'>
-          <Heading size={6} textColor='primary' caps>Typography</Heading>
-          <Heading size={1} textColor='secondary'>Heading 1</Heading>
-          <Heading size={2} textColor='secondary'>Heading 2</Heading>
-          <Heading size={3} textColor='secondary'>Heading 3</Heading>
-          <Heading size={4} textColor='secondary'>Heading 4</Heading>
-          <Heading size={5} textColor='secondary'>Heading 5</Heading>
-          <Text size={6} textColor='secondary'>Standard text</Text>
+        <Slide bgColor='secondary'>
+          <Heading size={1}>React</Heading>
         </Slide>
-        <Slide transition={['fade']} bgColor='primary' textColor='tertiary'>
-          <Heading size={6} textColor='secondary' caps>Standard List</Heading>
+        <Slide bgColor='tertiary' textColor='primary'>
+          <Heading size={1}>Material Design via React</Heading>
+        </Slide>
+        <Slide bgColor='primary'>
+          <Heading>Announcements</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Next Meeting: 3/14 @ 7PM</ListItem>
+            <ListItem>Next Board Meeting: 3/14 @ 6PM</ListItem>
           </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor='secondary' textColor='primary'>
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
         </Slide>
       </Deck>
     );
