0c6072553ddb36dc40eba9c8d9383e409f3fb20b
Nick Jarvis <najarvis2016@gmail.com>
2018-03-28 19:03:00 -0400

    About Me Page

--- Resources/Web Development Server/server.py
+++ Resources/Web Development Server/server.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
+@app.route('/about/<name>')
+@app.route('/about')
+def about(name=None):
+    if name is None:
+        return render_template("about_me_template.html")
+
+    if name == "Nick":
+        return render_template("about_nick.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
