ad8bcdb831e345053d50c7ecdde1302b5697e748
John Meyer <0x326@users.noreply.github.com>
2018-03-31 21:27:34 -0400

    Add live code for 3/28 meeting

--- Resources/Web Development Server/server.py
+++ Resources/Web Development Server/server.py
@@ -21,8 +21,8 @@ def about(name=None):
     if name is None:
         return render_template("about_me_template.html")
 
-    if name == "Nick":
-        return render_template("about_nick.html")
+    print("about_{0}.html".format(name))
+    return render_template("about_{0}.html".format(name), user=name)
 
 
 if __name__ == '__main__':
---
+++ Resources/Web Development Server/static/Me2.jpg.sh
@@ -0,0 +1,1 @@
+curl https://cdna.artstation.com/p/assets/images/images/008/542/808/large/wallace-silva-inaros-skin.jpg?1513438098 -o Me2.jpg
