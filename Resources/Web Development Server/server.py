c0bc6c949924efdaa37600ddf91ab05ddd57ddf2
John Meyer <0x326@users.noreply.github.com>
2018-04-03 16:58:47 -0400

    Open webbrowser on server start

--- Resources/Web Development Server/server.py
+++ Resources/Web Development Server/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import webbrowser
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -18,4 +19,5 @@ def about(name=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    webbrowser.open('http://localhost:5000/')
+    app.run(host='0.0.0.0', port=5000)
