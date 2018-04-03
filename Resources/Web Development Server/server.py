43c8d29d56e8321ef885521ce6c3879fa2d62a4e
John Meyer <0x326@users.noreply.github.com>
2018-04-03 16:50:17 -0400

    Cherry-pick changes to template and server from meeting
    Based on ad8bcdb831e345053d50c7ecdde1302b5697e748 with modifications

--- Resources/Web Development Server/server.py
+++ Resources/Web Development Server/server.py
@@ -14,8 +14,7 @@ def about(name=None):
     if name is None:
         return render_template("about_me_template.html")
 
-    if name == "Nick":
-        return render_template("about_nick.html")
+    return render_template("about_{0}.html".format(name), user=name)
 
 
 if __name__ == '__main__':
