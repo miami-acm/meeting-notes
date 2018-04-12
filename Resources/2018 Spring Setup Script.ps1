a55f5a578895eb1eee8d37a3a022e3b612c4c444
John Meyer <0x326@users.noreply.github.com>
2018-04-11 21:14:58 -0400

    Update scripts

--- Resources/2018 Spring Setup Script.ps1
+++ Resources/2018 Spring Setup Script.ps1
@@ -4,3 +4,15 @@ if (!(command scoop 2> $null)) {
     Set-ExecutionPolicy RemoteSigned -scope CurrentUser
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
+
+if (!(command py 2> $null) -and !(command python 2> $null)) {
+    # https://www.python.org/
+    echo "Installing Python 3"
+    scoop install python3
+}
+
+if (!(command pipenv 2> $null)) {
+    # https://www.python.org/
+    echo "Installing Pipenv"
+    pip3 install pipenv
+}
--- Resources/2018 Spring Setup Script.sh
+++ Resources/2018 Spring Setup Script.sh
@@ -3,3 +3,15 @@ if ! command -v brew > /dev/null ; then
     echo "Installing Homebrew"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+if ! command -v python3 > /dev/null ; then
+    # https://www.python.org/
+    echo "Installing Python 3"
+    brew install python3
+fi
+
+if ! command -v pipenv > /dev/null ; then
+    # https://docs.pipenv.org/
+    echo "Installing Pipenv"
+    pip3 install pipenv
+fi
