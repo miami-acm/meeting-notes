4e39ff68486fd05f954863913794a9e883aafb62
John Meyer <0x326@users.noreply.github.com>
2018-04-16 14:14:24 -0400

    Replace Powershell aliases with cmdlets

--- Resources/2018 Spring Setup Script (Windows).ps1
+++ Resources/2018 Spring Setup Script (Windows).ps1
@@ -2,31 +2,31 @@
 
 if (!(command scoop 2> $null)) {
     # http://scoop.sh/
-    echo "Installing Scoop"
+    Write-Output "Installing Scoop"
     Set-ExecutionPolicy RemoteSigned -scope CurrentUser
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 
 if (!(command py 2> $null) -and !(command python 2> $null)) {
     # https://www.python.org/
-    echo "Installing Python 3"
+    Write-Output "Installing Python 3"
     scoop install python3
 }
 
 if (!(command pipenv 2> $null)) {
     # https://docs.pipenv.org/
-    echo "Installing Pipenv"
+    Write-Output "Installing Pipenv"
     pip3 install pipenv
 }
 
 if (!(command pg_ctl 2> $null)) {
     # https://www.postgresql.org/
-    echo "Installing PostgreSQL"
+    Write-Output "Installing PostgreSQL"
     scoop install postgresql
 }
 
 if (!(command make 2> $null)) {
     # https://www.gnu.org/software/make/
-    echo "Installing GNU make"
+    Write-Output "Installing GNU make"
     scoop install make
 }
