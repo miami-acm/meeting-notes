1529feb9586a999d8838e02095f9be4ea4ad8dec
John Meyer <0x326@users.noreply.github.com>
2018-04-18 20:46:35 -0400

    Update Linux script

--- Resources/2018 Spring Setup Script (Debian).sh
+++ Resources/2018 Spring Setup Script (Debian).sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+sudo apt-get update
+
 if ! command -v python3 > /dev/null ; then
     # https://www.python.org/
     echo "Installing Python 3"
-    sudo apt install python3
+    sudo apt-get install python3 -y
 fi
 
 if ! command -v pipenv > /dev/null ; then
@@ -15,11 +17,11 @@ fi
 if ! command -v pg_ctl > /dev/null ; then
     # https://www.postgresql.org/
     echo "Installing PostgreSQL"
-    sudo apt install postgresql
+    sudo apt-get install postgresql -y
 fi
 
 if ! command -v make > /dev/null ; then
     # https://www.gnu.org/software/make/
     echo "Installing GNU make"
-    sudo apt install make
+    sudo apt-get install make -y
 fi
